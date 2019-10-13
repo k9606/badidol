5098d61d4875340d5507d2d2dbdd9c239663e9ab
k9606 <bishengfei@126.com>
2019-10-13 10:13:45 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="#">K9606</a> 设计和编码 <span
+                style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:bishengfei@126.com">联系我们</a></p>
+    </div>
+</footer>
