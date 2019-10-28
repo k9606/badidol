7f2aa4259e661992380517d3b02041d395e19f04
k9606 <bishengfei@126.com>
2019-10-28 14:08:57 +0800

    页脚 ok

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="footer">
-    <div class="container">
-        <p class="float-left">
-            由 <a href="#">BadIdol</a> 设计和编码 <span
-                style="color: #e27575;font-size: 14px;">❤</span>
-        </p>
-
-        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
-    </div>
-</footer>
+<div class="fly-footer">
+    <p>
+        <a href="#" target="_blank">BadIdol 坏偶像</a> 2019 &copy; <a href="#" target="_blank">badidol.com</a>
+    </p>
+    <p>
+        <a href="mailto:{{ setting('contact_email') }}">联系我们</a>
+        <a href="https://learnku.com/laravel" target="_blank">Laravel China</a>
+        <a href="https://www.layui.com" target="_blank">Layui</a>
+    </p>
+</div>
