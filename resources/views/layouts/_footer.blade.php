47889ab607c160b1a168a60e9425ace24c7b2011
k9606 <bishengfei@126.com>
2019-10-28 20:14:04 +0800

    侧边栏

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,8 @@
 <div class="fly-footer">
     <p>
-        <a href="#" target="_blank">BadIdol 坏偶像</a> 2019 &copy; <a href="#" target="_blank">badidol.com</a>
+        2019 &copy;<a href="#" target="_blank">badidol.com</a>
     </p>
     <p>
-        <a href="mailto:{{ setting('contact_email') }}">联系我们</a>
-        <a href="https://learnku.com/laravel" target="_blank">Laravel China</a>
-        <a href="https://www.layui.com" target="_blank">Layui</a>
+        <a href="mailto:{{ setting('contact_email') }}">联系 BadIdol 坏偶像</a>
     </p>
 </div>
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,42 +1,65 @@
-<div class="card ">
-    <div class="card-body">
-        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
-            <i class="fas fa-pencil-alt mr-2"></i> 新建帖子
-        </a>
-    </div>
-</div>
-
 @if (count($active_users))
-    <div class="card mt-4">
-        <div class="card-body active-users pt-2">
-            <div class="text-center mt-1 mb-0 text-muted">活跃用户</div>
-            <hr class="mt-2">
+    <div class="fly-panel fly-rank fly-rank-reply" id="LAY_replyRank">
+        <h3 class="fly-panel-title">活跃用户</h3>
+        <dl>
+            <!--<i class="layui-icon fly-loading">&#xe63d;</i>-->
             @foreach ($active_users as $active_user)
-                <a class="media mt-2" href="{{ route('users.show', $active_user->id) }}">
-                    <div class="media-left media-middle mr-2 ml-1">
-                        <img src="{{ $active_user->avatar }}" width="24px" height="24px" class="media-object">
-                    </div>
-                    <div class="media-body">
-                        <small class="media-heading text-secondary">{{ $active_user->name }}</small>
-                    </div>
-                </a>
+                <dd>
+                    <a href="user/home.html">
+                        <img
+                            src="{{ $active_user->avatar }}"><cite>{{ $active_user->name }}</cite><i>106次回答</i>
+                    </a>
+                </dd>
             @endforeach
-        </div>
+        </dl>
     </div>
 @endif
 
 @if (count($links))
-    <div class="card mt-4">
-        <div class="card-body pt-2">
-            <div class="text-center mt-1 mb-0 text-muted">资源推荐</div>
-            <hr class="mt-2 mb-3">
+    <dl class="fly-panel fly-list-one">
+        <dt class="fly-panel-title">资源推荐</dt>
+        @foreach ($links as $link)
+            <dd>
+                <a href="{{ $link->link }}">{{ $link->title }}</a>
+                <span><i class="iconfont icon-pinglun1"></i> 16</span>
+            </dd>
+    @endforeach
+
+    <!-- 无数据时 -->
+        <!--
+        <div class="fly-none">没有相关数据</div>
+        -->
+    </dl>
+@endif
+
+@if (count($links))
+    <div class="fly-panel">
+        <h3 class="fly-panel-title">资源推荐</h3>
+        <ul class="fly-panel-main fly-list-static">
             @foreach ($links as $link)
-                <a class="media mt-1" href="{{ $link->link }}">
-                    <div class="media-body">
-                        <span class="media-heading text-muted">{{ $link->title }}</span>
-                    </div>
-                </a>
+                <li>
+                    <a href="{{ $link->link }}" target="_blank">{{ $link->title }}</a>
+                </li>
             @endforeach
-        </div>
+        </ul>
     </div>
 @endif
+
+<div class="fly-panel">
+    <div class="fly-panel-title">
+        这里可作为广告区域
+    </div>
+    <div class="fly-panel-main">
+        <a href="#" target="_blank" class="fly-zanzhu" style="background-color: #5FB878;">BadIdol 坏偶像</a>
+    </div>
+</div>
+
+<div class="fly-panel fly-link">
+    <h3 class="fly-panel-title">友情链接</h3>
+    <dl class="fly-panel-main">
+        <dd><a href="https://learnku.com/laravel" target="_blank">Laravel China</a>
+        <dd>
+        <dd><a href="https://www.layui.com" target="_blank">Layui</a>
+        <dd>
+    </dl>
+</div>
