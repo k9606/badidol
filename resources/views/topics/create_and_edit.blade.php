8b67ffaaa24c5560a3c1e2cefb9a959644ec1844
k9606 <bishengfei@126.com>
2019-10-30 14:08:39 +0800

    创建与编辑

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -2,63 +2,69 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="col-md-10 offset-md-1">
-            <div class="card ">
-
-                <div class="card-body">
-                    <h2 class="">
-                        <i class="far fa-edit"></i>
-                        @if($topic->id)
-                            编辑话题
-                        @else
-                            新建话题
-                        @endif
-                    </h2>
-
-                    <hr>
-
-                    @if($topic->id)
-                        <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
-                            <input type="hidden" name="_method" value="PUT">
+    <div class="layui-container fly-marginTop">
+        <div class="fly-panel" pad20 style="padding-top: 5px;">
+            <!--<div class="fly-none">没有权限</div>-->
+            <div class="layui-form layui-form-pane">
+                <div class="layui-tab layui-tab-brief" lay-filter="user">
+                    <ul class="layui-tab-title">
+                        <li class="layui-this">
+                            @if($topic->id)
+                                编辑话题
                             @else
-                                <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
-                                    @endif
+                                新建话题
+                            @endif
+                        </li>
+                    </ul>
+                    <div class="layui-form layui-tab-content" id="LAY_ucm" style="padding: 20px 0;">
+                        <div class="layui-tab-item layui-show">
+                            @if($topic->id)
+                                <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
+                                    <input type="hidden" name="_method" value="PUT">
+                                    @else
+                                        <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
+                                            @endif
 
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                                    @include('shared._error')
-
-                                    <div class="form-group">
-                                        <input class="form-control" type="text" name="title"
-                                               value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <select class="form-control" name="category_id" required>
-                                            <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类
-                                            </option>
-                                            @foreach ($categories as $value)
-                                                <option
-                                                    value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
-                                                    {{ $value->name }}
-                                                </option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <textarea name="body" class="form-control" id="editor" rows="6"
-                                                  placeholder="请填入至少三个字符的内容。"
-                                                  required>{{ old('body', $topic->body ) }}</textarea>
-                                    </div>
-
-                                    <div class="well well-sm">
-                                        <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2"
-                                                                                         aria-hidden="true"></i> 保存
-                                        </button>
-                                    </div>
-                                </form>
+                                            @include('shared._error')
+                                            <div class="layui-row layui-col-space15 layui-form-item">
+                                                <div class="layui-col-md3">
+                                                    <label class="layui-form-label">所在分类</label>
+                                                    <div class="layui-input-block">
+                                                        <select lay-verify="required" name="category_id" lay-filter="column">
+                                                            <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类
+                                                            </option>
+                                                            @foreach ($categories as $value)
+                                                                <option
+                                                                    value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
+                                                                    {{ $value->name }}
+                                                                </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="layui-col-md9">
+                                                    <label for="L_title" class="layui-form-label">标题</label>
+                                                    <div class="layui-input-block">
+                                                        <input type="text" id="L_title" name="title" required lay-verify="required"
+                                                               autocomplete="off" class="layui-input" value="{{ old('title', $topic->title ) }}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="layui-form-item layui-form-text">
+                                                <div class="layui-input-block">
+                                    <textarea id="L_content" name="body" required lay-verify="required"
+                                              placeholder="详细描述" class="layui-textarea fly-editor"
+                                              style="height: 260px;">{{ old('body', $topic->body ) }}</textarea>
+                                                </div>
+                                            </div>
+                                            <div class="layui-form-item">
+                                                <button class="layui-btn" lay-filter="*" lay-submit>立即发布</button>
+                                            </div>
+                                        </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
