ae85bc9daa85bb45f260e0cf06c754f916c975b9
k9606 <bishengfei@126.com>
2019-10-30 13:34:13 +0800

    回复样式

--- resources/views/topics/_reply_list.blade.php
+++ resources/views/topics/_reply_list.blade.php
@@ -1,46 +1,50 @@
-<ul class="list-unstyled">
+<ul class="jieda" id="jieda">
     @foreach ($replies as $index => $reply)
-        <li class=" media" name="reply{{ $reply->id }}" id="reply{{ $reply->id }}">
-            <div class="media-left">
-                <a href="{{ route('users.show', [$reply->user_id]) }}">
-                    <img class="media-object img-thumbnail mr-3" alt="{{ $reply->user->name }}"
-                         src="{{ $reply->user->avatar }}" style="width:48px;height:48px;"/>
+        <li data-id="111" class="jieda-daan">
+            <a name="item-1111111111"></a>
+            <div class="detail-about detail-about-reply">
+                <a class="fly-avatar" href="{{ route('users.show', [$reply->user_id]) }}">
+                    <img
+                        src="{{ $reply->user->avatar }}"
+                        alt="{{ $reply->user->name }}">
                 </a>
-            </div>
-
-            <div class="media-body">
-                <div class="media-heading mt-0 mb-1 text-secondary">
-                    <a href="{{ route('users.show', [$reply->user_id]) }}" title="{{ $reply->user->name }}">
-                        {{ $reply->user->name }}
+                <div class="fly-detail-user">
+                    <a href="{{ route('users.show', [$reply->user_id]) }}" class="fly-link">
+                        <cite>{{ $reply->user->name }}</cite>
                     </a>
-                    <span class="text-secondary"> • </span>
-                    <span class="meta text-secondary"
-                          title="{{ $reply->created_at }}">{{ $reply->created_at->diffForHumans() }}</span>
+                    <!--
+                    <span style="color:#5FB878">(管理员)</span>
+                    <span style="color:#FF9E3F">（社区之光）</span>
+                    <span style="color:#999">（该号已被封）</span>
+                    -->
+                </div>
 
-                    {{-- 回复删除按钮 --}}
-                    @can('destroy', $reply)
-                        <span class="meta float-right">
-              <form action="{{ route('replies.destroy', $reply->id) }}"
-                    onsubmit="return confirm('确定要删除此评论？');"
-                    method="post">
-                {{ csrf_field() }}
-                  {{ method_field('DELETE') }}
-                <button type="submit" class="btn btn-default btn-xs pull-left text-secondary">
-                  <i class="far fa-trash-alt"></i>
-                </button>
-              </form>
-            </span>
-                    @endcan
+                <div class="detail-hits">
+                    <span>{{ $reply->created_at->diffForHumans() }}</span>
                 </div>
-                <div class="reply-content text-secondary">
-                    {!! $reply->content !!}
-                </div>
+            </div>
+            <div class="detail-body jieda-body photos">
+                {!! $reply->content !!}
+            </div>
+            <div class="jieda-reply">
+              <span class="jieda-zan" type="zan">
+                <i class="iconfont icon-zan"></i>
+                <em>66</em>
+              </span>
+                <span type="reply">
+                <i class="iconfont icon-svgmoban53"></i>
+                回复
+              </span>
+                @can('destroy', $reply)
+                    <div class="jieda-admin">
+                        <span type="del">删除</span>
+                        <!-- <span class="jieda-accept" type="accept">采纳</span> -->
+                    </div>
+                @endcan
             </div>
         </li>
+@endforeach
 
-        @if ( ! $loop->last)
-            <hr>
-        @endif
-
-    @endforeach
+<!-- 无数据时 -->
+    <!-- <li class="fly-none">消灭零回复</li> -->
 </ul>
