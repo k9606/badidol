1c2eacd04a61c9427fe4d88e725c93567a0edb71
k9606 <bishengfei@126.com>
2019-10-30 12:17:24 +0800

    回复样式

--- resources/views/topics/_reply_box.blade.php
+++ resources/views/topics/_reply_box.blade.php
@@ -1,13 +1,19 @@
 @include('shared._error')
 
-<div class="reply-box">
+<div class="layui-form layui-form-pane">
     <form action="{{ route('replies.store') }}" method="POST" accept-charset="UTF-8">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="hidden" name="topic_id" value="{{ $topic->id }}">
-        <div class="form-group">
-            <textarea class="form-control" rows="3" placeholder="分享你的见解~" name="content"></textarea>
+        <div class="layui-form-item layui-form-text">
+            <a name="comment"></a>
+            <div class="layui-input-block">
+                                <textarea id="L_content" name="content" required lay-verify="required"
+                                          placeholder="请输入内容" class="layui-textarea fly-editor"
+                                          style="height: 150px;"></textarea>
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-share mr-1"></i> 回复</button>
+        <div class="layui-form-item">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input type="hidden" name="topic_id" value="{{ $topic->id }}">
+            <button class="layui-btn" lay-filter="*" lay-submit>提交回复</button>
+        </div>
     </form>
 </div>
-<hr>
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -5,73 +5,69 @@
 
 @section('content')
 
-    <div class="row">
+    <div class="layui-row layui-col-space15">
+        <div class="layui-col-md8 content detail">
+            <div class="fly-panel detail-box">
+                <h1>{{ $topic->title }}</h1>
+                <div class="fly-detail-info">
+                    <!-- <span class="layui-badge">审核中</span> -->
+                    <span class="layui-badge layui-bg-green fly-detail-column">动态</span>
 
-        <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
-            <div class="card ">
-                <div class="card-body">
-                    <div class="text-center">
-                        作者：{{ $topic->user->name }}
-                    </div>
-                    <hr>
-                    <div class="media">
-                        <div align="center">
-                            <a href="{{ route('users.show', $topic->user->id) }}">
-                                <img class="thumbnail img-fluid" src="{{ $topic->user->avatar }}" width="300px"
-                                     height="300px">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    <span class="layui-badge" style="background-color: #999;">未结</span>
+                    <!-- <span class="layui-badge" style="background-color: #5FB878;">已结</span> -->
 
-        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-            <div class="card">
-                <div class="card-body">
-                    <h1 class="text-center mt-3 mb-3">
-                        {{ $topic->title }}
-                    </h1>
-
-                    <div class="article-meta text-center text-secondary">
-                        {{ $topic->created_at->diffForHumans() }}
-                        ⋅
-                        <i class="far fa-comment"></i>
-                        {{ $topic->reply_count }}
-                    </div>
-
-                    <div class="topic-body mt-4 mb-4">
-                        {!! $topic->body !!}
-                    </div>
+                    <span class="layui-badge layui-bg-black">置顶</span>
+                    <span class="layui-badge layui-bg-red">精帖</span>
 
                     @can('update', $topic)
-                        <div class="operate">
-                            <hr>
-                            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm"
-                               role="button">
-                                <i class="far fa-edit"></i> 编辑
-                            </a>
-                            <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
-                                  style="display: inline-block;"
-                                  onsubmit="return confirm('您确定要删除吗？');">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <button type="submit" class="btn btn-outline-secondary btn-sm">
-                                    <i class="far fa-trash-alt"></i> 删除
-                                </button>
-                            </form>
+                        <div class="fly-admin-box" data-id="123">
+                            <span class="layui-btn layui-btn-xs jie-admin" type="del"><a
+                                    href="{{ route('topics.edit', $topic->id) }}">删除</a></span>
+                            <span class="layui-btn layui-btn-xs jie-admin" type="del"><a
+                                    href="{{ route('topics.destroy', $topic->id) }}">删除</a></span>
+
+                            <span class="layui-btn layui-btn-xs jie-admin" type="set" field="stick" rank="1">置顶</span>
+                            <!-- <span class="layui-btn layui-btn-xs jie-admin" type="set" field="stick" rank="0" style="background-color:#ccc;">取消置顶</span> -->
+
+                            <span class="layui-btn layui-btn-xs jie-admin" type="set" field="status" rank="1">加精</span>
+                            <!-- <span class="layui-btn layui-btn-xs jie-admin" type="set" field="status" rank="0" style="background-color:#ccc;">取消加精</span> -->
                         </div>
                     @endcan
-
+                    <span class="fly-list-nums">
+            <a href="#comment"><i class="iconfont" title="回答">&#xe60c;</i> {{ $topic->reply_count }}</a>
+          </span>
+                </div>
+                <div class="detail-about">
+                    <a class="fly-avatar" href="{{ route('users.show', $topic->user->id) }}">
+                        <img src="{{ $topic->user->avatar }}"
+                             alt="{{ $topic->user->name }}">
+                    </a>
+                    <div class="fly-detail-user">
+                        <a href="{{ route('users.show', $topic->user->id) }}" class="fly-link">
+                            <cite>{{ $topic->user->name }}</cite>
+                        </a>
+                        <span>{{ $topic->created_at->diffForHumans() }}</span>
+                    </div>
+                    <div class="detail-hits" id="LAY_jieAdmin" data-id="123">
+                        <span style="padding-right: 10px; color: #FF7200">暂无签名</span>
+                        @can('update', $topic)
+                            <span class="layui-btn layui-btn-xs jie-admin" type="edit"><a
+                                    href="{{ route('topics.edit', $topic->id) }}">编辑此贴</a></span>
+                        @endcan
+                    </div>
+                </div>
+                <div class="detail-body photos">
+                    {!! $topic->body !!}
                 </div>
             </div>
 
             {{-- 用户回复列表 --}}
-            <div class="card topic-reply mt-4">
-                <div class="card-body">
-                    @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
-                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
-                </div>
+            <div class="fly-panel detail-box" id="flyReply">
+                <fieldset class="layui-elem-field layui-field-title" style="text-align: center;">
+                    <legend>回帖</legend>
+                </fieldset>
+                @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
+                @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
             </div>
 
         </div>
