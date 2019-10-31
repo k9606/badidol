21f365f7cbb3e7d0dab459e9b01e15e6b2fbe060
k9606 <bishengfei@126.com>
2019-10-31 15:06:55 +0800

    临时屏蔽提示

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -7,7 +7,7 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function saving(User $user)
+    public function saved(User $user)
     {
         // 如果用户没有头像, 生成默认头像
         if (!$user->avatar) {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
 <div class="layui-container">
 
-    @include('shared._messages')
+{{--    @include('shared._messages')--}}
 
     @yield('content')
 
