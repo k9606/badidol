4e0dfe396a3cfe39f258c8820f06a8c863e0abcf
k9606 <bishengfei@126.com>
2019-11-05 18:38:09 +0800

    autofocus

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -59,7 +59,7 @@
                                                 <div class="layui-input-block">
                                     <textarea name="body" class="form-control" id="editor" rows="6"
                                               placeholder="请填入至少三个字符的内容。"
-                                              required>{{ old('body', $topic->body ) }}</textarea>
+                                              required autofocus>{{ old('body', $topic->body ) }}</textarea>
                                                 </div>
                                             </div>
                                             <div class="layui-form-item">
