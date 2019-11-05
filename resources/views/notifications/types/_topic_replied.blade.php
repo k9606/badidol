67c2b75ed17db136f23b230c8eafcb43e61f60c4
k9606 <bishengfei@126.com>
2019-11-05 18:17:58 +0800

    回复页面

--- resources/views/notifications/types/_topic_replied.blade.php
+++ resources/views/notifications/types/_topic_replied.blade.php
@@ -1,8 +1,10 @@
 <li>
     <p>
-        {{ $notification->data['user_name'] }}
-        <span>{{ $notification->created_at->diffForHumans() }}</span>
-        评论了<a href="{{ $notification->data['topic_link'] }}" target="_blank">{{ $notification->data['topic_title'] }}</a>
+        <a href="{{ route('users.show', $notification->data['user_id']) }}"
+           target="_blank">{{ $notification->data['user_name'] }}</a><i class="iconfont icon-renzheng"
+                                                                        title="认证信息：XXX"></i>
+        <span>{{ $notification->created_at->diffForHumans() }}评论了</span>
+        <a href="{{ $notification->data['topic_link'] }}" target="_blank">{{ $notification->data['topic_title'] }}</a>
     </p>
     <div class="home-dacontent">
         {!! $notification->data['reply_content'] !!}
