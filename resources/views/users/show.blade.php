76c96437f63dae00d54f7e8570fc0c4bc6f24b63
k9606 <bishengfei@126.com>
2019-11-05 16:32:26 +0800

    个人页面

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,57 +3,30 @@
 @section('title', $user->name . ' 的个人中心')
 
 @section('content')
+    <div class="fly-home fly-panel" style="background-image: url();">
+        <img src="{{ $user->avatar }}" alt="{{ $user->name }}">
+        <i class="iconfont icon-renzheng" title="社区认证"></i>
+        <h1>
+            {{ $user->name }}
+            <i class="iconfont icon-nan"></i>
+            <!-- <i class="iconfont icon-nv"></i>  -->
+        </h1>
 
-    <div class="row">
+        <p style="padding: 10px 0; color: #5FB878;">{{ $user->email }}</p>
 
-        <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
-            <div class="card ">
-                <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
-                <div class="card-body">
-                    <h5><strong>个人简介</strong></h5>
-                    <p>{{ $user->introduction }}</p>
-                    <hr>
-                    <h5><strong>注册于</strong></h5>
-                    <p>{{ $user->created_at->diffForHumans() }}</p>
-                    <hr>
-                    <h5><strong>最后活跃</strong></h5>
-                    <p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-            <div class="card ">
-                <div class="card-body">
-                    <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
-                </div>
-            </div>
-            <hr>
+        <p class="fly-home-info">
+            <i class="iconfont icon-shijian"></i><span>注册于 {{ $user->created_at->diffForHumans() }}</span>
+            <i class="iconfont icon-chengshi"></i><span>最后活跃 {{ $user->last_actived_at->diffForHumans() }}</span>
+        </p>
 
-            {{-- 用户发布的内容 --}}
-            <div class="card ">
-                <div class="card-body">
-                    <ul class="nav nav-tabs">
-                        <li class="nav-item">
-                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}"
-                               href="{{ route('users.show', $user->id) }}">
-                                Ta 的话题
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}"
-                               href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">
-                                Ta 的回复
-                            </a>
-                        </li>
-                    </ul>
-                    @if (if_query('tab', 'replies'))
-                        @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
-                    @else
-                        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
-                    @endif
-                </div>
-            </div>
+        <p class="fly-home-sign">{{ $user->introduction }}</p>
 
-        </div>
     </div>
+
+    @if (if_query('tab', 'replies'))
+        @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+    @else
+        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+    @endif
+
 @stop
